9ccbc14d7d5e02b1f976a1495d94d55c32799ac4
gnehs <jayuiop@gmail.com>
2021-02-25 00:23:47 +0800

    added axios retry

--- crawler/fetchSinglePage.js
+++ crawler/fetchSinglePage.js
@@ -4,15 +4,35 @@ const axios = require('axios').default;
 const iconv = require('iconv-lite');
 const axiosRetry = require('axios-retry');
 axiosRetry(axios, { retries: 10, shouldResetTimeout: true });
-async function fetchSinglePage(url) {
-    const resp = await axios.request({
-        method: 'GET',
-        url,
-        responseType: 'arraybuffer',
-        reponseEncoding: 'binary'
+const delay = (s) => {
+    return new Promise(resolve => {
+        setTimeout(resolve, s);
     });
+};
+async function fetchSinglePage(url) {
+    const resp = await getResp(url)
     const html = iconv.decode((await resp.data), "big5");
     return cheerio.load(html);
+}
+async function getResp(url, retry = 0) {
+    try {
+        const resp = await axios.request({
+            method: 'GET',
+            url,
+            responseType: 'arraybuffer',
+            reponseEncoding: 'binary'
+        })
+        return resp
+    }
+    catch (e) {
+        if (retry < 10) {
+            retry += 1
+            await delay(1000 * retry);
+            return getResp(url, retry)
+        } else {
+            console.log(`[error] ${url}`)
+        }
+    }
 }
 
 module.exports = { fetchSinglePage };
