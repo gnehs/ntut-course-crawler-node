904fbe7752984437c2286b99f526dd5338cda72d
gnehs <jayuiop@gmail.com>
2021-02-24 22:47:11 +0800

    updated fetchAll.yaml

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ const fetchYearSem = require('./crawler/fetchYearSem');
 const { fetchDepartment } = require('./crawler/fetchDepartment');
 
 async function main() {
-    let { year, sem } = (await fetchYearSem()).current
-    console.log(year, sem)
-    //await fetchDepartment(year, sem)
+    let year, sem
+    if (process.argv.length > 3) {
+        year = process.argv[2]
+        sem = process.argv[3]
+    }
+    else {
+        let { y, s } = (await fetchYearSem()).courseDescriptionData
+        year = y
+        sem = s
+    }
+    await fetchDepartment(year, sem)
     for (let m of ['日間部', '進修部', '研究所(日間部、進修部、週末碩士班)']) {
         await fetchCourse(m, year, sem)
     }
