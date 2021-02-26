5e24e520cf8d9004975a68ed643554cd99e7c698
gnehs <jayuiop@gmail.com>
2021-02-26 12:10:56 +0800

    fixed course

--- crawler/fetchCourse.js
+++ crawler/fetchCourse.js
@@ -44,7 +44,7 @@ async function fetchCourse(matricKey = '日間部', year = 109, sem = 2) {
     }
 
     console.log(`[fetch] 正在取得${matricKey}課程列表...`)
-    let keyword = ''//'%A4%E9'
+    let keyword = ''// '%A4%E9'
     let url = `https://aps.ntut.edu.tw/course/tw/QueryCourse.jsp?stime=101%2C102%2C103%2C104%2C122%2C105%2C106%2C107%2C108%2C109%2C110%2C111%2C112%2C113%2C201%2C202%2C203%2C204%2C222%2C205%2C206%2C207%2C208%2C209%2C210%2C211%2C212%2C213%2C301%2C302%2C303%2C304%2C322%2C305%2C306%2C307%2C308%2C309%2C310%2C311%2C312%2C313%2C401%2C402%2C403%2C404%2C422%2C405%2C406%2C407%2C408%2C409%2C410%2C411%2C412%2C413%2C501%2C502%2C503%2C504%2C522%2C505%2C506%2C507%2C508%2C509%2C510%2C511%2C512%2C513%2C601%2C602%2C603%2C604%2C622%2C605%2C606%2C607%2C608%2C609%2C610%2C611%2C612%2C613&year=${year}&matric=${encodeURI(iconv.encode(matric[matricKey], "big5"))}&sem=${sem}&unit=**&cname=${keyword}&ccode=&tname=&D0=ON&D1=ON&D2=ON&D3=ON&D4=ON&D5=ON&D6=ON&P1=ON&P2=ON&P3=ON&P4=ON&PN=ON&P5=ON&P6=ON&P7=ON&P8=ON&P9=ON&P10=ON&P11=ON&P12=ON&P13=ON&search=%B6%7D%A9l%ACd%B8%DF`
     let $ = await fetchSinglePage(url)
     $('tr:first-child').remove()
@@ -98,7 +98,7 @@ async function fetchCourse(matricKey = '日間部', year = 109, sem = 2) {
             syllabusLinks: parseSyllabusLinks($($(this).children('td')[20]).children('a')),
         })
     }).toArray()
-
+    let result = []
     let coursesDone
 
     console.log(`[fetch] ${matricKey} course description`)
@@ -108,12 +108,14 @@ async function fetchCourse(matricKey = '日間部', year = 109, sem = 2) {
             let courseDescriptionData = await fetchCourseDescription(x.courseDescriptionLink)
             coursesDone++
             console.log(`[fetch] course description (${coursesDone}/${courseData.length}) ${matricKey} - ${courseDescriptionData.name.zh} done.`)
-            return { ...courseDescriptionData, ...x }
+            result.push({ ...courseDescriptionData, ...x })
         }
         catch (e) {
             console.log(`[error][fetch] course description ${matricKey} - ${courseDescriptionData.name.zh} error.`)
         }
     }
+    console.log(`[fetch] ${matricKey == '日間部' ? 'main' : matricKey}.json saved.`)
+    jsonfile.writeFileSync(`./dist/${year}/${sem}/${matricKey == '日間部' ? 'main' : matricKey}.json`, result)
 
     console.log(`[fetch] ${matricKey} syllabus`)
     coursesDone = 0
@@ -134,7 +136,6 @@ async function fetchCourse(matricKey = '日間部', year = 109, sem = 2) {
 
 
     console.log(`[fetch] ${matricKey} done.`)
-    jsonfile.writeFileSync(`./dist/${year}/${sem}/${matricKey == '日間部' ? 'main' : matricKey}.json`, courseData)
 }
 
 module.exports = { fetchCourse };
