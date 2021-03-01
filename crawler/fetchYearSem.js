55ba3bfbf223bd48e854650ef0c6b07e6fca16df
gnehs <jayuiop@gmail.com>
2021-03-01 14:40:00 +0800

    updated fetchYearSem

--- crawler/fetchYearSem.js
+++ crawler/fetchYearSem.js
@@ -5,16 +5,23 @@ async function main() {
     let url = 'https://aps.ntut.edu.tw/course/tw/QueryCurrPage.jsp'
     let $ = await fetchSinglePage(url)
 
-    let result = { years: {}, current: { year: 0, sem: 0 } }
+    let result = {
+        years: {}, current: {
+            year: $('select[name="year"] option:selected').text(),
+            sem: $('select[name="sem"] option:selected').text() == '上學期' ? 1 : 2
+        }
+    }
+
     $('select[name="year"] option')
         .map(function () { return $(this).text() })
         .toArray()
-        .map(x => { result.years[x] = [1, 2] })
-    result.current = {
-        year: $('select[name="year"] option:selected').text(),
-        sem: $('select[name="sem"] option:selected').text() == '上學期' ? 1 : 2
-    }
-    console.log('::set-output name=matrix::', JSON.stringify(Object.keys(result.years)))
+        .map(x => {
+            if (x == result.current.year && 1 == result.current.sem) {
+                result.years[x] = [1]
+            } else {
+                result.years[x] = [1, 2]
+            }
+        })
     fs.mkdirSync(`./dist/`, { recursive: true });
     jsonfile.writeFileSync(`./dist/main.json`, result.years)
     return result
