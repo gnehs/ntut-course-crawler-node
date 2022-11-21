944f8146ce5798a125e0d12135c8c1fb3034762d
gnehs <jayuiop@gmail.com>
2022-11-21 15:42:11 +0800

    refactor fetchSyllabus

--- crawler/fetchSyllabus.js
+++ crawler/fetchSyllabus.js
@@ -1,5 +1,6 @@
 const { fetchSinglePage } = require('./fetchSinglePage')
 const jsonfile = require('jsonfile');
+const cheerioTableparser = require('cheerio-tableparser');
 const axios = require('axios').default;
 const axiosRetry = require('axios-retry');
 const pangu = require('./tools/pangu').spacing;
@@ -33,63 +34,65 @@ async function fetchSyllabus(matricKey = '日間部', year = 109, sem = 2) {
   }
 }
 
-async function fetchSyllabusData(url = 'ShowSyllabus.jsp?snum=292267&code=11710') {
+async function fetchSyllabusData(url = 'ShowSyllabus.jsp?snum=305082&code=23885') {
   let $ = await fetchSinglePage('https://aps.ntut.edu.tw/course/tw/' + url)
-  let res = {
-    name: $('body > p:nth-child(3) > table > tbody > tr:nth-child(1) > th:nth-child(2)').text(),
-    email: $('body > p:nth-child(3) > table > tbody > tr:nth-child(2) > th:nth-child(2)').text().trim().replace(globalRegexParse, ''),
-    latestUpdate: $('body > p:nth-child(3) > table > tbody > tr:nth-child(3) > th:nth-child(2)').text(),
-    objective: pangu($('body > p:nth-child(3) > table > tbody > tr:nth-child(4) > td > textarea').html().replace(/<br\s*[\/]?>/gi, "\n").replace(/\t/gi, "　　")),
-    schedule: pangu($('body > p:nth-child(3) > table > tbody > tr:nth-child(5) > td > textarea').html().replace(/<br\s*[\/]?>/gi, "\n").replace(/\t/gi, "　　")),
-    scorePolicy: pangu($('body > p:nth-child(3) > table > tbody > tr:nth-child(6) > td > textarea').html().replace(/<br\s*[\/]?>/gi, "\n").replace(/\t/gi, "　　")),
-    materials: pangu($('body > p:nth-child(3) > table > tbody > tr:nth-child(7) > td > textarea').html().replace(/<br\s*[\/]?>/gi, "\n").replace(/\t/gi, "　　")),
-    foreignLanguageTextbooks: !!$('body > p:nth-child(3) > table > tbody > tr:nth-child(7) > td').text().match(/使用外文原文書：是/),
+  cheerioTableparser($)
+  let data = $(`body > p:nth-child(3) > table`).parsetable(true, true, true)
+  let dataKeyMap = {
+    '教師姓名': `name`,
+    'Email': `email`,
+    '最後更新時間': `latestUpdate`,
+    '課程大綱': `objective`,
+    '課程進度': `schedule`,
+    '評量方式與標準': `scorePolicy`,
+    '使用教材、參考書目或其他': `materials`,
+    '課程諮詢管道': `consultation`,
+    '備註': `remarks`
   }
-  // remove name ends with "　教師諮商時間(Office Hours)"
+  let res = {}
+  for (let key of data[0]) {
+    let index = data[0].indexOf(key)
+    let remappingKey = dataKeyMap[key] ?? key
+    res[remappingKey] = data[1][index]
+  }
   res.name = res.name.replace(/　教師諮商時間\(Office Hours\)/, '')
-  // get remarks
+  res.foreignLanguageTextbooks = !!res.materials.match(/使用外文原文書：是/)
+  res.materials = res.materials.replace(/(.+)使用外文原文書：(.+)\n/, '')
+  res.remarks = res.remarks.replace(/因應疫情發展，本學期教學及授課方式請依照學校網頁所公布之訊息為準：\n/, '')
+  res.remarks = res.remarks.replace(/\(https(.+)tw\)\n/, '')
+
+  for (let key of Object.keys(res)) {
+    if (typeof res[key] == 'string') {
+      res[key] = pangu(res[key].replace(/^ +/g, ''))
+    }
+  }
+
+  // 110 年上學期
   let remarksText = $('body > p:nth-child(3) > table > tbody > tr:nth-child(8) > td').text()
-  if (remarksText != '') {
-    if (remarksText.match('\n本學期課程因應疫情警戒等級規劃上課方式原則如下，實際實施日期與上課方式')) {
-      let remarks = $('body > p:nth-child(3) > table > tbody > tr:nth-child(8) > td').html().replace(/<br\s*[\/]?>/gi, "\n").replace(/\t/gi, "　　")
+  if (remarksText != '' && remarksText.match('\n本學期課程因應疫情警戒等級規劃上課方式原則如下，實際實施日期與上課方式')) {
+    let remarks = $('body > p:nth-child(3) > table > tbody > tr:nth-child(8) > td').html().replace(/<br\s*[\/]?>/gi, "\n").replace(/\t/gi, "　　")
 
-      let covidDatas = []
-      $('body > p:nth-child(3) > table > tbody > tr:nth-child(8) > td > div').each(function (index, element) {
-        covidDatas.push($(element).html().replace(/<br\s*[\/]?>/gi, "\n").replace(/\t/gi, "　　"));
-      });
-      covidDatas = covidDatas.map(x => x == ' ' || x == '' ? null : x)
+    let covidDatas = []
+    $('body > p:nth-child(3) > table > tbody > tr:nth-child(8) > td > div').each(function (index, element) {
+      covidDatas.push($(element).html().replace(/<br\s*[\/]?>/gi, "\n").replace(/\t/gi, "　　"));
+    });
+    covidDatas = covidDatas.map(x => x == ' ' || x == '' ? null : x)
 
-      let covid19 = {
-        // if lv2
-        lv2Description: pangu(covidDatas[0]),
-        courseScoreMethod: pangu(covidDatas[1]),
-        // If distance learning or triage is implemented at the beginning of the semester
-        courseInfo: pangu(covidDatas[2]),
-        courseURL: pangu(covidDatas[3]),
-        contactInfo: pangu(covidDatas[4]),
-        additionalInfo: pangu(covidDatas[5]),
-      }
-      try {
-        covid19.lv2Method = pangu(remarks.match(/<b>●上課方式：<\/b>(.+)\n/)[1])
-      } catch (e) {
-
-      }
-      res.covid19 = covid19
+    let covid19 = {
+      // if lv2
+      lv2Description: pangu(covidDatas[0]),
+      courseScoreMethod: pangu(covidDatas[1]),
+      // If distance learning or triage is implemented at the beginning of the semester
+      courseInfo: pangu(covidDatas[2]),
+      courseURL: pangu(covidDatas[3]),
+      contactInfo: pangu(covidDatas[4]),
+      additionalInfo: pangu(covidDatas[5]),
     }
-    else {
-      res.remarks = pangu($('body > p:nth-child(3) > table > tbody > tr:nth-child(8) > td').html()
-        .replace(/<br\s*[\/]?>/gi, "\n")
-        .replace(/color=#ff0000/gi, '')
-        .replace(/color=#ff0000/g, '')
-        .replace(/style="(.+)"/gi, '')
-        .replace(/\t/gi, "　　"))
-        // remove <b></b>
-        .replace(/<b>/gi, '')
-        .replace(/<\/b>/gi, '')
-    }
-
+    try {
+      covid19.lv2Method = pangu(remarks.match(/<b>●上課方式：<\/b>(.+)\n/)[1])
+    } catch (e) { }
+    res.covid19 = covid19
   }
   return res
 }
-
 module.exports = { fetchSyllabus };
