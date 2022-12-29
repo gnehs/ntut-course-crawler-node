1e182554162583cf7e2e1dd958ab6ff92403ecbd
gnehs <jayuiop@gmail.com>
2022-12-29 15:52:11 +0800

    added fetchCalendar

---
+++ crawler/fetchCalendar.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const ical = require('node-ical')
+const pangu = require('pangu')
+let url = 'https://calendar.google.com/calendar/ical/docfuhim9b22fqvp2tk842ak3c%40group.calendar.google.com/public/basic.ics'
+async function fetchCalendar() {
+  console.log('fetch calerdar data');
+  let data = Object.entries(await ical.async.fromURL(url))
+    .filter(([key, value]) => key != 'vcalendar')
+    .map(([_, x]) => x)
+    // short by start
+    .sort((a, b) => new Date(a.start) - new Date(b.start))
+    .map(x => {
+      console.log(x)
+      x.summary = pangu.spacing(x.summary)
+      return x
+    })
+  fs.mkdirSync('./dist/', { recursive: true })
+  fs.writeFileSync('./dist/calendar.json', JSON.stringify(data))
+}
+module.exports = { fetchCalendar };
